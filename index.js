811845287bd5a3bb05d45d82353a2937b39b9559
marito <marioceballosossa@gmail.com>
2023-09-01 11:29:03 -0500

    show how to use a collaborative repository
    Look how easy is to use collaborative repositories with other people
    (aka cristian :p)

---
+++ index.js
@@ -0,0 +1,8 @@
+export function rootLayout () {
+    return (
+        <div>
+            <h1>This is an example of how using git</h1>
+            <p>This is pretty fun :)</p>
+        </div>
+    )
+}
